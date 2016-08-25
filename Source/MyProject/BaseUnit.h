08d1dfb295efbf0d33f13ac5e0ded6c6cda9a645
Josh <joshuajames355@gmail.com>
2016-08-25 18:10:44 +0100

    Added basic unit and movement test

---
+++ Source/MyProject/BaseUnit.h
@@ -0,0 +1,37 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/Actor.h"
+#include "BaseUnit.generated.h"
+
+UCLASS()
+class MYPROJECT_API ABaseUnit : public ACharacter
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	ABaseUnit();
+
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+	
+	// Called every frame
+	virtual void Tick( float DeltaSeconds ) override;
+	
+	
+	UPROPERTY(EditDefaultsOnly, Category = Units)
+	int32 TeamNumber;
+	
+	UPROPERTY(EditDefaultsOnly, Category = Units)
+	float StartingHealth;
+	
+	UPROPERTY(BlueprintReadOnly, Category = Units)
+	float CurrentHealth;
+	
+	UFUNCTION(BlueprintCallable,BlueprintImplementableEvent , Category = Units)
+	void MoveTo(FVector Target);
+	
+	
+};
